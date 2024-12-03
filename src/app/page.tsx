ed1a9338fdb592da14eed2bc699c9cf42669e4b9
P A Chathura Madhava <chathuratechdev@gmail.com>
2024-12-03 09:55:04 +0530

    build: Add develop branch to the repository

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Rent a Car Application</h1>
+      {/* Add Develop Branch to the Repository */}
     </main>
   );
 }
