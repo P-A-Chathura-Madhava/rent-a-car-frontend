03f4768b9012a5de9a18540fb00443df6bf9d4d2
P A Chathura Madhava <chathuratechdev@gmail.com>
2024-12-11 18:40:49 +0530

    Style: Add About Us and Featured Cars Sections

--- src/ui-core/templates/pages/HomePage.tsx
+++ src/ui-core/templates/pages/HomePage.tsx
@@ -1,6 +1,8 @@
 import Navbar from "@/ui-core/components/organisms/Navbar"
+import AboutUsSection from "@/ui-core/layouts/AboutUsSection"
 import BookCardSection from "@/ui-core/layouts/BookCardSection"
 import BookingStepsSection from "@/ui-core/layouts/BookingStepsSection"
+import FeaturedCars from "@/ui-core/layouts/FeaturedCars"
 import WelcomeSection from "@/ui-core/layouts/WelcomeSection"
 
 function HomePage() {
@@ -10,6 +12,8 @@ function HomePage() {
         <WelcomeSection />
         <BookCardSection />
         <BookingStepsSection />
+        <AboutUsSection />
+        <FeaturedCars />
     </div>
   )
 }
